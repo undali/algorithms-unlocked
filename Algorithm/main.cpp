0c24c33b79601cfd93c24c6ac2ef483a90415bd5
Al Mamun <t@Als-MacBook-Pro.local>
2019-02-20 23:27:54 +0600

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Al Mamun on 20/2/19.
+//  Copyright © 2019 Al Mamun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
